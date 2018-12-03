6287ec6e4a7f2964fb9bcddd98b15e9576f0cecc
migueldcferreira <44262368+migueldcferreira@users.noreply.github.com>
2018-12-03 19:24:42 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php 
+$textform=$_POST["testtext"];
+$liste= array("rhume", "gastro", "gastro-entérite", "pneumonie");
+$i=0;
+$listeMotsTrouves=array();
+$nbMotsTrouves=0;
+while($i<4)
+{
+	if(strpos($textform, $liste[$i])!==false)
+	{
+		echo("le mot " . $liste[$i] . " a ete trouve dans votre chaine de caractere");
+
+		//On ajoute le mot de notre $liste[] dans la liste des mots trouvées 
+		$listeMotsTrouves[]=$liste[$i];
+		echo "</br>";
+		echo($listeMotsTrouves[$nbMotsTrouves]);
+		$nbMotsTrouves++;
+	}
+
+
+
+	$i++;
+}
+?>
