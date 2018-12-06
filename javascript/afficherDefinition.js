24e87bf75e728794b9e167d0c60c51c522fc8e85
Theo Delacoux <theo.dx@hotmail.fr>
2018-12-06 13:31:44 +0100

    Mise a jour afficherDefinition.js

--- javascript/afficherDefinition.js
+++ javascript/afficherDefinition.js
@@ -2,7 +2,8 @@ var vocab = getElementsByClassName('vocabulaire');
 for(let i = 0; i < vocab.length; i++) {
   vocab[i].addEventListener("click", function(e)
   {
-    var def = e.children[1].innerHTML;
+    var def = e.children[0].innerHTML;
+    alert("Bonjour");
     alert(def);
   })
 }
