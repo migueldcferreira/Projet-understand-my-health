94eb7c71afb460273798aa0402c2992fc490d21a
Theo Delacoux <theo.dx@hotmail.fr>
2018-12-06 12:49:29 +0100

    Ajout lien dans menu.php

--- php/menu.php
+++ php/menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-	<a class="navbar-brand" href="#"><i class="fas fa-file-medical-alt"></i> Docteur Traducteur</a>
+	<a class="navbar-brand" href="accueil.php"><i class="fas fa-file-medical-alt"></i> Docteur Traducteur</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
@@ -7,7 +7,7 @@
 	<div class="collapse navbar-collapse" id="navbarColor01">
 		<ul class="navbar-nav mr-auto">
 			<li class="nav-item active">
-				<a class="nav-link" href="#">Traduire un contenu médical<span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="accueil.php">Traduire un contenu médical<span class="sr-only">(current)</span></a>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href="#">Dictionnaire</a>
