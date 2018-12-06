52cfbfb30e8375396f4f510d0ccfee86f9076cbe
hugomartinshm <44262443+hugomartinshm@users.noreply.github.com>
2018-12-06 11:01:47 +0100

    Update accueil.php

--- php/accueil.php
+++ php/accueil.php
@@ -12,19 +12,19 @@
       <div class="container">
         <div class="row">
           <!--Copier-Coller-->
-          <form class="form col-md-6" action="test.php" method="post">
+          <form class="form col-md-12" action="test.php" method="post">
             <label for="myText" class="form-control-label"><i class="fas fa-paste"></i> Copiez-collez un texte médical</label>
             <textarea class="form-control form-control-block" id="myText" name="testtext"></textarea>
             <button type="submit" class="btn btn-primary">Traduire</button>
             <button type="reset" class="btn btn-secondary">Effacer</button>
           </form>
-          <form class="form col-md-6">
+          <form class="form col-md-12">
             <label for="myInputFile" class="form-control-label"><i class="fas fa-file-pdf"></i> Importez un fichier</label>
             <input type="file" class="form-control-file" id="myInputFile" aria-describedby="fileHelp">
             <small id="fileHelp" class="form-text text-muted">Vous pouvez importer un fichier texte (.txt) ou un fichier PDF (.pdf). Dans le cas d'un PDF, veillez à ce qu'il ne soit pas protégé en auquel cas il sera impossible de traiter le document.</small>
             <button type="submit" class="btn btn-primary">Valider</button>
           </form>
-          <form class="form col-md-6">
+          <form class="form col-md-12
             <label for="myURL" class="form-control-label"><i class="fas fa-code"></i> Renseignez directement le lien d'une page avec du contenu médical</label>
             <div class="form-inline">
               <input type="text" class="form-control form-control-lg" id="myURL"></input>
