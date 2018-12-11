b11bf87552e43f43b860b118f3217040e4184569
Yannis Slaouti <yannis.sj22@gmail.com>
2018-12-11 17:29:33 +0100

    Bootstrap_et_animations

--- php/menu.php
+++ php/menu.php
@@ -1,32 +1,48 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-success">
-	<a class="navbar-brand" href="accueil.php"><i class="fas fa-file-medical-alt"></i> Docteur Traducteur</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
 
-	<div class="collapse navbar-collapse" id="navbarColor01">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="accueil.php">Traduire un contenu médical<span class="sr-only">(current)</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Dictionnaire</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Devenir membre</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">À propos</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="maj.php">Mettre à jour</a>
-			</li>
-		</ul>
-		<form class="form-inline my-2 my-lg-0">
-			<input class="form-control mr-sm-2 form-control-sm" type="text" placeholder="Identifiant">
-			<input class="form-control mr-sm-2 form-control-sm" type="password" placeholder="Mot de passe">
-			<button class="btn btn-secondary my-2 my-sm-0 btn-sm" type="submit"><i class="fas fa-user-md"></i> Se connecter</button>
-		</form>
 
-	</div>
-</nav>
+<div id="topheader">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-success " role="navigation">
+		<div class="container-fluid">
+			
+				<a class="navbar-brand" href="accueil.php"><i class="fas fa-file-medical-alt"></i> Docteur Traducteur</a>
+				<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+
+				   <span class="icon-bar top-bar"></span>
+				   <span class="icon-bar middle-bar"></span>
+				   <span class="icon-bar bottom-bar"></span>
+				   <span class="sr-only">Toggle navigation</span>
+				</button>
+				
+				
+			
+
+			<div class="navbar-collapse collapse" id="navbar">
+				<ul class="nav navbar-nav mr-auto">
+					<li class="nav-item " data-toggle="collapse" data-target="#navbar-collapse.in">
+						<a href="accueil.php" class="nav-link">Traduire un contenu médical<span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
+						<a href="#top" class="nav-link">Dictionnaire</a>
+					</li>
+					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
+						<a href="register.php" class="nav-link">Devenir membre</a>
+					</li>
+					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
+						<a href="#top" class="nav-link">À propos</a>
+					</li>
+					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
+						<a href="maj.php" class="nav-link">Mettre à jour</a>
+					</li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2 form-control-sm" type="text" placeholder="Identifiant">
+					<input class="form-control mr-sm-2 form-control-sm" type="password" placeholder="Mot de passe">
+					<button class="btn btn-secondary my-2 my-sm-0 btn-sm" type="submit"><i class="fas fa-user-md"></i> Se connecter</button>
+				</form>
+
+			</div>
+		</div>
+	</nav>
+</div>
+
+
---
+++ php/test.php
@@ -0,0 +1,50 @@
+<?php 
+	$textform=$_POST["testtext"];
+	$liste= array("rhume", "gastro", "gastro-entérite", "pneumonie");
+	$i=0;
+	$listeMotsTrouves=array();
+	$nbMotsTrouves=0;
+
+	$mot = strtok($textform, " \n\t");
+
+	while ($mot !== false)
+	{
+		echo "Word=$mot<br />";
+		while($i<4)
+		{
+			if (strcasecmp($mot, $liste[$i])==0)
+			{
+				echo("le mot " . $liste[$i] . " a ete trouve dans votre chaine de caractere <br />");
+				//On ajoute le mot de notre $liste[] dans la liste des mots trouvées 
+				$listeMotsTrouves[]=$liste[$i];
+				echo("on place le mot dans une liste de mots trouves" . $listeMotsTrouves[$nbMotsTrouves] . "<br />");
+				$nbMotsTrouves++;
+			}
+			$i++;
+		}
+
+		$mot = strtok(" \n\t");
+		$i=0;
+	}
+
+
+
+	/*
+	********
+	Test 1
+	********
+	while($i<4)
+	{
+		if(strpos($textform, $liste[$i])!==false)
+		{
+			echo("le mot " . $liste[$i] . " a ete trouve dans votre chaine de caractere");
+
+			//On ajoute le mot de notre $liste[] dans la liste des mots trouvées 
+			$listeMotsTrouves[]=$liste[$i];
+			echo "</br>";
+			echo($listeMotsTrouves[$nbMotsTrouves]);
+			$nbMotsTrouves++;
+		}
+		$i++;
+	}*/
+?>
