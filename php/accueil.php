e3493f74f3e9dab38c1714ddb32c4eed8788b707
Yannis Slaouti-Jégou <yannis.sj22@gmail.com>
2018-12-11 18:23:26 +0100

    test_enlevé

--- php/accueil.php
+++ php/accueil.php
@@ -47,31 +47,17 @@
 										  <div class="form-group">
 											<input type="file" name="files[]" id="js-upload-files" multiple>
 										  </div>
-										  <button type="submit" class="btn btn-sm btn-primary" id="js-upload-submit">Upload files</button>
+										  <button type="submit" class="btn btn-sm btn-primary" id="js-upload-submit">Importez</button>
 										</div>
 									  </form>
 
 									  <!-- Drop Zone -->
-									  <h4>Or drag and drop files below</h4>
+									  <h4>Ou glissez-déposez ici</h4>
 									  <div class="upload-drop-zone" id="drop-zone">
-										Just drag and drop files here
+										Glissez-déposez ici
 									  </div>
 
-									  <!-- Progress Bar -->
-									  <div class="progress">
-										<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-										  <span class="sr-only">60% Complete</span>
-										</div>
-									  </div>
-
-									  <!-- Upload Finished -->
-									  <div class="js-upload-finished">
-										<h3>Processed files</h3>
-										<div class="list-group">
-										  <a href="#" class="list-group-item list-group-item-success"><span class="badge alert-success pull-right">Success</span>image-01.jpg</a>
-										  <a href="#" class="list-group-item list-group-item-success"><span class="badge alert-success pull-right">Success</span>image-02.jpg</a>
-										</div>
-									  </div>
+									 
 									</div>
 								  </div>
 								</div> <!-- /container -->
