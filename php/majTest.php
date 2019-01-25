1af348c8feb4038b381a367fc6f3adbc1321fd6a
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-01-25 18:15:54 +0100

    Update majTest.php

--- php/majTest.php
+++ php/majTest.php
@@ -1,6 +1,6 @@
-<? php
-	$monfichier  =  fopen ( ' ../../maj.txt ' , ' r + ' );
-	fputs ( $monfichier , ' 2 ' );
-	fclose ( $monfichier );
-	en-tête ( ' location: accueil.php ' );
-? >
+<?php
+	$monfichier = fopen('../../maj.txt', 'r+');
+	fputs($monfichier, '1');
+	fclose($monfichier);
+	header('location:accueil.php');
+?>
