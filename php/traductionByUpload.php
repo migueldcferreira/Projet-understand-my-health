281a0d397b3dcfad001d49d7cce61b3769bc5ff1
migueldcferreira <44262368+migueldcferreira@users.noreply.github.com>
2019-01-27 23:24:18 +0100

    MAJ du fichier traduction.php + ajout fichier traductionByURL/ByUpload et de la class PdfToText

--- php/traductionByUpload.php
+++ php/traductionByUpload.php
@@ -11,19 +11,41 @@
       <?php include("menu.php"); ?>
 
 <?php
-	$textform=$_POST["testtext"];
+	/*****
+		Copyright (c) 2016, Christian Vigh.
+		All rights reserved.
+
+		J'utilise la classe "PdfToText.phpclass" appartenant à Christian Vigh pour pouvoir obtenir le texte provenant d'un fichier pdf grâce à la l'instanciation d'un
+		objet de la classe PdfToText.
+	******/
+	include ( 'PdfToText.phpclass' );
+	$extension = substr($_FILES['fichier']['name'], -3, 3);
+	if ($extension == 'pdf') 
+	{
+		$pdf 	=  new PdfToText($_FILES["fichier"]["tmp_name"]) ;
+		$pdf = str_replace( array( '?', ',', '.', ':', '!', '\''), ' ', $pdf ); 
+
+		$mot = strtok($pdf, " \n\t.");
+	}
+	elseif ($extension == 'txt')
+	{
+		$textform = file_get_contents($_FILES["fichier"]["tmp_name"]);
+		$textform = str_replace( array( '?', ',', '.', ':', '!', '\''), ' ', $textform ); 
+		$mot = strtok($textform, " \n\t.");
+	}
+
 	$liste= array("rhume", "gastro", "gastro-entérite", "pneumonie");
 	$definition= array("Le rhume est causé par un virus. C'est une infection fréquente du nez et de la gorge nommée aussi rhinite virale ou aiguë.",
-										"Abréviation de gastro-entérite, maladie qui résulte de l'inflammation de l'estomac ou des intestins due à un virus. Le malade souffre alors d'une diarrhée plus ou moins sévère.",
-									 	"Gastro-entérite désigne une inflammation simultanée de la muqueuse de l'estomac et de celle des intestins.",
-										" La pneumonie est une infection des poumons causée le plus souvent par un virus ou une bactérie.");
+					   "Abréviation de gastro-entérite, maladie qui résulte de l'inflammation de l'estomac ou des intestins due à un virus. Le malade souffre alors d'une diarrhée plus ou moins sévère.",
+					   "Gastro-entérite désigne une inflammation simultanée de la muqueuse de l'estomac et de celle des intestins.",
+					   " La pneumonie est une infection des poumons causée le plus souvent par un virus ou une bactérie.");
+	
+
+	
 	$i=0;
 	$listeMotsTrouves=array();
 	$nbMotsTrouves=0;
 
-	$textform = str_replace( array( '?', ',', '.', ':', '!', '\''), ' ', $textform ); 
-
-	$mot = strtok($textform, " \n\t");
 
 	while ($mot !== false)
 	{
@@ -52,30 +74,10 @@
 		{
 			echo "$mot ";
 		}
-		$mot = strtok(" \n\t");
+		$mot = strtok(" \n\t.");
 		$i=0;
 	}
 
-
-
-	/*
-	********
-	Test 1
-	********
-	while($i<4)
-	{
-		if(strpos($textform, $liste[$i])!==false)
-		{
-			echo("le mot " . $liste[$i] . " a ete trouve dans votre chaine de caractere");
-
-			//On ajoute le mot de notre $liste[] dans la liste des mots trouv�es
-			$listeMotsTrouves[]=$liste[$i];
-			echo "</br>";
-			echo($listeMotsTrouves[$nbMotsTrouves]);
-			$nbMotsTrouves++;
-		}
-		$i++;
-	}*/
 ?>
 
 		</div>
