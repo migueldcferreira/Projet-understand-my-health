b042bd8238f952fdf820a282002b73c029fa0b4c
migueldcferreira <44262368+migueldcferreira@users.noreply.github.com>
2019-01-29 16:51:44 +0100

    Suppression balise "fluid"

--- php/traductionByUpload.php
+++ php/traductionByUpload.php
@@ -7,7 +7,6 @@
   <body>
 
     <!--Container principal-->
-    <div class="container-fluid">
       <?php include("menu.php"); ?>
 
 <?php
@@ -80,7 +79,6 @@
 
 ?>
 
-		</div>
 		<?php include("script_menu.php"); ?>
 		<script type="text/javascript" src="../javascript/afficherDefinition.js"></script>
 	</body>
