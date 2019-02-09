c1461ada2638a8deae92a3fe8618f654b80cff64
AnassOus <44498953+AnassOus@users.noreply.github.com>
2019-02-09 20:49:05 +0100

    Add files via upload
    gerer les membres

--- php/admin_liste_membres.php
+++ php/admin_liste_membres.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
 
+
 	 <?php include("head.php"); ?>
     <link rel="stylesheet" href="..\css/choosetrad.css">
 
@@ -10,6 +11,30 @@
 
 	
 	<?php include("menu_admin.php"); ?>
+	<?php
+
+
+	require('Bdd.php');
+
+	try
+	{
+		$bdd = Bdd::connect("BDD_TRADOCTEUR");
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Permet de récuperer une exception lorsque il y a une erreur au niveau de la base de donnée.
+																	   //On pourra donc traiter l'erreur plus simplement avec un try et catch.
+		
+
+		$sql_adm = "SELECT  ID_UTILISATEUR,NOM, PRENOM, ADRESSE_MAIL,RANG,DATE_DERNIERE_CONNEXION FROM TABLE_UTILISATEUR WHERE ACTIF = 1 AND RANG LIKE '%admin' " ; //requête pour trouver les définitions non validées (propositions)
+		$res_adm = $bdd->query($sql_adm);
+		$sql_mem = "SELECT  ID_UTILISATEUR,NOM, PRENOM, ADRESSE_MAIL,RANG,DATE_DERNIERE_CONNEXION FROM TABLE_UTILISATEUR WHERE ACTIF = 1 AND RANG LIKE '%membre%' " ; //requête pour trouver les définitions non validées (propositions)
+		$res_mem = $bdd->query($sql_mem);
+
+
+	}
+	catch (Exception $e)
+	{
+			die('Erreur : ' . $e->getMessage());
+	}?>
+
 
 	<section id="tabs" class="project-tab">
 		<div class="container">
@@ -19,96 +44,71 @@
 						<thead>
 							<tr>
 								<th scope="col">Administrateurs</th>
-								<th scope="col">Identifiant</th>
+								<th scope="col">Nom</th>
+								<th scope="col">Prénom</th>
+								<th scope="col">Adresse mail</th>
 								<th scope="col">Rang</th>
 								<th scope="col">Dernière connexion</th>
 								<th scope="col"></th>
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<th scope="row">1</th>
-								<td>Admin 1</td>
-								<td>super-admin</td>
-								<td style="opacity: 0.75">26/01/2019 11:55</td>
-								<td><button class="btn btn-danger btn-sm tooltipsAdmin disabled" title="Supprimer cet administrateur"><i class="fas fa-minus-circle"></i></button></td>
-							</tr>
-							<tr>
-								<th scope="row">2</th>
-								<td>Admin2</td>
-								<td>admin</td>
-								<td style="opacity: 0.75">13/12/2018 23:27</td>
-								<td><button class="btn btn-danger btn-sm tooltipsAdmin" title="Supprimer cet administrateur"><i class="fas fa-minus-circle"></i></button></td>
-							</tr>
-							<tr>
-								<th scope="row">3</th>
-								<td>Admin3</td>
-								<td>admin</td>
-								<td style="opacity: 0.75">25/01/2019 9:02</td>
-								<td><button class="btn btn-danger btn-sm tooltipsAdmin" title="Supprimer cet administrateur"><i class="fas fa-minus-circle"></i></button></td>
-							</tr>
-						</tbody>
+																			<?php $i=1;
+																			 while ($row = $res_adm->fetch()) { ?>
+																				<tr>
+																						<td ><?php printf ("%d", $i); ?>  </td>
+																						<td> <?php printf ("%s", $row[1]); ?> </td>
+																						<td> <?php printf ("%s", $row[2]); ?> </td>
+																						<td> <?php printf ("%s", $row[3]); ?> </td>
+																						<td> <?php printf ("%s", $row[4]); ?> </td>
+																						<td> <?php printf ("%s", $row[5]); ?> </td>
+																						<td><button class="btn btn-danger btn-sm tooltipsAdmin disabled" title="Supprimer cet administrateur"><i class="fas fa-minus-circle"></i></button></td>
+																				</tr>
+																			<?php $i++;} ?>
+																		</tbody>
+					
 					</table>
 				</div>
 			</div>
 			<div class="row">
                 <div class="col-md-12">
                 	<table class="table">
-						<thead>
+                		<thead>
 							<tr>
 								<th scope="col">Membres</th>
-								<th scope="col">Identifiant</th>
+								<th scope="col">Nom</th>
+								<th scope="col">Prénom</th>
+								<th scope="col">Adresse mail</th>
 								<th scope="col">Rang</th>
 								<th scope="col">Dernière connexion</th>
 								<th scope="col"></th>
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<th scope="row">1</th>
-								<td>Membre 1</td>
-								<td>membre</td>
-								<td style="opacity: 0.75">15/12/2018 2:14</td>
-								<td>
-									<button class="btn btn-success btn-sm tooltipsAdmin" title="Promouvoir comme membre spécialiste"><i class="fas fa-user-plus"></i></button>
-									<button class="btn btn-warning btn-sm tooltipsAdmin disabled" title="Rétrograder à membre"><i class="fas fa-user-minus"></i></button>
-									<button class="btn btn-danger btn-sm tooltipsAdmin" title="Supprimer ce membre"><i class="fas fa-minus-circle"></i></button>
-								</td>
-							</tr>
-							<tr>
-								<th scope="row">2</th>
-								<td>Membre 2</td>
-								<td>membre spécialiste</td>
-								<td style="opacity: 0.75">29/12/2018 20:31</td>
-								<td>
-									<button class="btn btn-success btn-sm tooltipsAdmin disabled" title="Promouvoir comme membre spécialiste"><i class="fas fa-user-plus"></i></button>
-									<button class="btn btn-warning btn-sm tooltipsAdmin" title="Rétrograder à membre"><i class="fas fa-user-minus"></i></button>
-									<button class="btn btn-danger btn-sm tooltipsAdmin" title="Supprimer ce membre"><i class="fas fa-minus-circle"></i></button>
-								</td>
-							</tr>
-							<tr>
-								<th scope="row">3</th>
-								<td>Membre 3</td>
-								<td>membre</td>
-								<td style="opacity: 0.75">06/01/2019 15:42</td>
-								<td>
-									<button class="btn btn-success btn-sm tooltipsAdmin" title="Promouvoir comme membre spécialiste"><i class="fas fa-user-plus"></i></button>
-									<button class="btn btn-warning btn-sm tooltipsAdmin disabled" title="Rétrograder à membre"><i class="fas fa-user-minus"></i></button>
-									<button class="btn btn-danger btn-sm tooltipsAdmin" title="Supprimer ce membre"><i class="fas fa-minus-circle"></i></button>
-								</td>
-							</tr>
-							<tr>
-								<th scope="row">4</th>
-								<td>Membre 4</td>
-								<td>membre</td>
-								<td style="opacity: 0.75">16/01/2019 19:08</td>
-								<td>
-									<button class="btn btn-success btn-sm tooltipsAdmin" title="Promouvoir comme membre spécialiste"><i class="fas fa-user-plus"></i></button>
-									<button class="btn btn-warning btn-sm tooltipsAdmin disabled" title="Rétrograder à membre"><i class="fas fa-user-minus"></i></button>
-									<button class="btn btn-danger btn-sm tooltipsAdmin" title="Supprimer ce membre"><i class="fas fa-minus-circle"></i></button>
-								</td>
-							</tr>
+																			<?php $i=1;
+																			 while ($row = $res_mem->fetch()) { ?>
+																				<tr>
+																						<td><?php printf ("%d", $i); ?>  </td>
+																						<td> <?php printf ("%s", $row[1]); ?> </td>
+																						<td> <?php printf ("%s", $row[2]); ?> </td>
+																						<td> <?php printf ("%s", $row[3]); ?> </td>
+																						<td> <?php printf ("%s", $row[4]); ?> </td>
+																						<td> <?php printf ("%s", $row[5]); ?> </td>
+																							<td>
+																								<a href="promotion.php?id=<?php echo $row[0] ?>">
+																								<button class="btn btn-success btn-sm tooltipsAdmin" title="Promouvoir comme membre spécialiste"><i class="fas fa-user-plus"></i>
+																								</button></a>
+																								<a href="retro.php?id=<?php echo $row[0] ?>">
+																								<button class="btn btn-warning btn-sm tooltipsAdmin disabled" title="Rétrograder à membre"><i class="fas fa-user-minus"></i>
+																								</button></a>
+																								<a href="supprimer_us.php?id=<?php echo $row[0] ?>">
+																								<button class="btn btn-danger btn-sm tooltipsAdmin" title="Supprimer ce membre"><i class="fas fa-minus-circle"></i></button></a>
+																							</td>
+																					
+																				</tr>
+																			<?php $i++;} ?>
 						</tbody>
+						
 					</table>
                 </div>
             </div>
