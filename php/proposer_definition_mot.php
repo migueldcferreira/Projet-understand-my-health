9829038c5e014e625894100f3e5cc8db2b012cd1
migueldcferreira <44262368+migueldcferreira@users.noreply.github.com>
2019-02-10 16:37:54 +0100

    ajout proposer_definition_mot.php

---
+++ php/proposer_definition_mot.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <?php
+		include('Bdd_inscription.php');
+		include('verif_membre.php');
+		include("head.php"); 
+	?>
+    <link rel="stylesheet" href="..\css/choosetrad.css">
+  </head>
+  <body>
+	<?php include("menu.php"); ?>
+
+	<div class="header form_head bg-success">
+  		<h2> Proposer l'ajout d'un nouveau mot</h2>
+	</div>
+	
+	<form method="post" action="proposer_definition_mot.php" class = "formulaire_stylise">
+  	<?php include('errors.php');?>
+  	<div class="input-group">
+  		<label>Mot : </label>
+  		<input type="text" name="MOT" >
+  	</div>
+  	<div class="input-group">
+  		<label>Definition : </label>
+  		<input type="text" name="DEFINITION">
+  	</div>
+  	<div class="input-group form-group">
+  		<button type="submit" class="btn btn-success btn-sm"  name="proposerDef">Proposer</button>
+  	</div>
+	
+	
+	
+	
+	<?php include("script_menu.php"); ?>
+
+  </body>
+</html>
