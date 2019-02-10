8507c71d42ed9f4873ba46dca687bbb30ec35f43
migueldcferreira <44262368+migueldcferreira@users.noreply.github.com>
2019-02-10 16:10:35 +0100

    séparation fonctionnalités visiteur, membre, admin
    +bouton déconnecter et bonjour (prenom utilisateur)

--- php/menu.php
+++ php/menu.php
@@ -24,18 +24,39 @@
 					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
 						<a href="#top" class="nav-link">Dictionnaire</a>
 					</li>
+					
+					
+					<?php if (!empty($_SESSION['username'])): ?>
+					
+					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
+						<a href="proposer_definition_mot.php" class="nav-link">Proposer l'ajout d'un nouveau mot</a>
+					</li>
+
+					<?php else: ?>
+
 					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
 						<a href="register.php" class="nav-link">Devenir membre</a>
 					</li>
 					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
 						<a href="login.php" class="nav-link">Se connecter</a>
 					</li>
+
+					<?php endif ?>
+					
+					
+					</li>
 					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
 						<a href="#top" class="nav-link">À propos</a>
 					</li>
+	
+					
+					<?php if (!empty($_SESSION['username']) && $_SESSION['rang']=="super-admin"): ?>
 					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
 						<a href="admin_liste_membres.php" class="nav-link">Espace Admin</a>
 					</li>
+					<?php endif ?>
+				
+				
 					<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
 						<div class="btn-group">
   							<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -48,6 +69,17 @@
   							</div>
 						</div>
 					</li>
+				
+					<li>
+						Bonjour <?php if(!empty($_SESSION['prenom'])) {echo $_SESSION['prenom'];}
+							      else {echo "Visiteur";}?>
+					</li>
+					
+					<?php if (!empty($_SESSION['username'])): ?>
+					<li>
+						<a href="deconnexion.php" class="btn btn-secondary my-2 my-sm-0 btn-sm"><i class="fas fa-caret-right"></i> Se déconnecter</a>
+					</li>
+					<?php endif ?>
 					<!--<li class="nav-item" data-toggle="collapse" data-target="#navbar-collapse.in">
 						<a href="maj.php" class="nav-link">Mettre à jour</a>
 					</li>-->
