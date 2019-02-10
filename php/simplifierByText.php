2a54a0e7f95ca464ae4400d6f32b506e4e10cddd
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-02-10 15:43:20 +0100

    Add files via upload

---
+++ php/simplifierByText.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <?php include("head.php"); ?>
+		<link rel="stylesheet" href="../css/traduction.css" />
+  </head>
+  <body>
+
+    <!--Container principal-->
+    <?php include("menu.php"); ?>
+    <?php
+    	if(empty($_POST["testtext"]))
+    	{
+    		die("Veuillez insérer du texte à simplifier");
+    	}
+    	$textForm=str_split($_POST["testtext"]);
+    	include("simplifier.php");
+    	simplifierTexteBrut($textForm,0);
+    ?>
+
+	  <?php include("script_menu.php"); ?>
+		<script type="text/javascript" src="../javascript/afficherDefinition.js"></script>
+	</body>
+</html>
