797123a89d458c012e6162b262c3a372fa060869
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-02-10 16:40:17 +0100

    Update simplifierByText.php

--- php/simplifierByText.php
+++ php/simplifierByText.php
@@ -9,19 +9,13 @@
     <!--Container principal-->
     <?php include("menu.php"); ?>
     <?php
-	echo "1";
     	if(empty($_POST["testtext"]))
     	{
     		die("Veuillez insérer du texte à simplifier");
     	}
-	echo "2";
     	$textForm=str_split($_POST["testtext"]);
-	echo "2";
     	require_once("simplifier.php");
-	echo "2";
     	simplifierTexteBrut($textForm,0);
-	echo "3";
-	echo "Fin du texte simplifié";
     ?>
 
 	  <?php include("script_menu.php"); ?>
