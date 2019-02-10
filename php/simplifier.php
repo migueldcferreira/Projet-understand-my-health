9b5cb2d22652279b99b0785d55eb0a87b4f63bd0
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-02-10 15:15:16 +0100

    Fonction de simplification d'un texte brut

---
+++ php/simplifier.php
@@ -0,0 +1,70 @@
+<?php
+//$balise =
+//0: on cherche des mots difficiles dans les balises (<>),
+//1: on supprime les balises et leur contenue,
+//2: on laisse les balises mais on ne cherche pas de mots difficiles dedans
+  function simplifierTexteBrut($text, $balise)
+  {
+    //connexion a la base de donnees
+    require('Bdd.php');
+    try
+    {
+      $bdd = Bdd::connect("BDD_TRADOCTEUR");
+      $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      //Permet de récuperer une exception lorsque il y a une erreur au niveau de la base de donnée.
+      //On pourra donc traiter l'erreur plus simplement avec un try et catch.
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    $mot = "";
+    $nbBaliseOuvrante = 0;
+    foreach($text as $lettre)
+    {
+      if($nbBaliseOuvrante==0 && preg_match("#[a-zA-Z]#",$lettre) || (strlen($mot)>0 && preg_match("#-#",$lettre)))
+      {
+        $mot .= $lettre;
+      }
+      else
+      {
+        if(strlen($mot) > 0)
+        {
+          $sql = "SELECT DEFINITION FROM TABLE_DEFINITION WHERE MOT LIKE '$mot';";
+      		$res = $bdd->query($sql); //On récupère (s'il en existe) les lignes de notre table "produits" qui répondent à notre requête $sql.
+      								  //Ces lignes sont stockées dans la variables $res qui est un tableau du jeu de résultat de notre requête.
+      		if(!empty($row = $res->fetch())) //La méthode fetch() permet de récupérer la ligne suivante du résultat de notre requete qui sont stockés dans la variable $res.
+      		{
+      			//while (!empty($row))
+      			//{
+      				echo '<span class="vocabulaire">
+      							<span class="expression">'.$mot.'</span>
+      							<span class="definition hidden">'.$row['DEFINITION'].'</span>
+      						</span>';
+      			//$row = $res->fetch();
+      			//}
+      		}
+      		else
+      		{
+      			echo "$mot";
+      		}
+        }
+        $mot = "";
+        if($balise>0 && $lettre === "<")
+        {
+          $nbBaliseOuvrante += 1;
+        }
+        elseif($balise>0 && $nbBaliseOuvrante > 0 && $lettre === ">")
+        {
+          $nbBaliseOuvrante -= 1;
+        }
+        else
+        {
+          if(!($balise == 1) || !($nbBaliseOuvrante>0)
+            echo "$lettre";
+        }
+      }
+    }
+  }
+?>
