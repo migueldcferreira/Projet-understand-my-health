40133e60b45548540e5c8d769e2ff1951dcce692
SlaoutiYannis <44498934+SlaoutiYannis@users.noreply.github.com>
2019-02-12 19:15:58 +0100

    erff

--- php/dictionnaire.php
+++ php/dictionnaire.php
@@ -3,15 +3,14 @@
   <head>
     <?php session_start();
     include("head.php"); ?>
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+    <script src="jquery-3.3.1.min.js"></script>
     <script>
     $(document).ready( function () {
     $('#table_id').DataTable();
     } ); </script>
-       <script src="jquery-3.3.1.min.js"></script>
 
     
     <link rel="stylesheet" href="..\css/choosetrad.css">
@@ -21,7 +20,7 @@
   </head>
   <body>
 
- 
+
 	
 <table id="table_id" class="display">
     <thead>
@@ -32,7 +31,7 @@
     </thead>
     <tbody>
         <tr>
-            <td>Row 1 Data 1 a</td>
+            <td>Row 1 Data 1</td>
             <td>Row 1 Data 2</td>
         </tr>
         <tr>
@@ -42,6 +41,25 @@
     </tbody>
 </table>
 
+  <?php include("menu.php"); 
+
+    require('Bdd.php');
+
+    try
+    {
+      $bdd = Bdd::connect("BDD_TRADOCTEUR");
+      $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Permet de récuperer une exception lorsque il y a une erreur au niveau de la base de donnée.
+                                       //On pourra donc traiter l'erreur plus simplement avec un try et catch.
+
+      $sql_base = "SELECT ID_DEFINITION, MOT, DEFINITION, DATE_AJOUT FROM TABLE_DEFINITION WHERE A_CONFIRMER = 0"; //requête pour trouver les définitions validées
+      $res_base = $bdd->query($sql_base);
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+  ?>
 
   <table id = "table" class="display">
     <thead>
