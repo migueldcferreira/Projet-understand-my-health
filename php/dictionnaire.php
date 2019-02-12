7b70e52a81acad7ae45ff36c98b63e595dfe421c
SlaoutiYannis <44498934+SlaoutiYannis@users.noreply.github.com>
2019-02-12 17:14:03 +0100

    test_brut

--- php/dictionnaire.php
+++ php/dictionnaire.php
@@ -38,22 +38,24 @@
 
   ?>
 	
-  <table id = "table_id" class="display">
+<table id="table_id" class="display">
     <thead>
         <tr>
-            <th scope="col">Mot</th>
-            <th scope="col">Définition</th>
-            <th scope="col">Date_ajout</th>
-          </tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+        </tr>
     </thead>
     <tbody>
-            <tr>
-                 <td> test </td>
-                 <td> test </td>
-                 <td> 2018-11-11</td>
-            </tr>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
     </tbody>
-  </table>
+</table>
 
 
   <table id = "table" class="display">
