68cfbd43a02a4ed6eb20927073414dd9c9a8ef68
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-02-13 19:19:23 +0100

    Update simplifierByURL.php

--- php/simplifierByURL.php
+++ php/simplifierByURL.php
@@ -17,7 +17,7 @@
 	//on recupere le texte a l'URL
 	$textform = file_get_contents($_POST["testurl"]);
 	//on garde seulement le body de la page
-      	$textForm = str_replace(array("#^.*<body[^>]*>#","#</body>.*$#") , "", $textform);
+      	$textForm = str_replace(array("#^[^ø]*<body[^>]*>#","#</body>[^ø]*$#") , "", $textform);
 	echo $textForm;
   
     	require_once("simplifier.php");
