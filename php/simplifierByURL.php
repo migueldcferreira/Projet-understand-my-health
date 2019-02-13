81776792c71e686046c2537aa8c56dd20a64781c
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-02-13 18:38:10 +0100

    Update simplifierByURL.php

--- php/simplifierByURL.php
+++ php/simplifierByURL.php
@@ -14,10 +14,11 @@
     	{
     		die("Veuillez insérer une URL");
     	}
-      $textForm = str_replace(array("#.*<body>#","#</body>.*#") , "", file_get_contents($_POST["testurl"]);
-    	require_once("simplifier.php");
+      	$textForm = str_replace(array("#.*<body>#","#</body>.*#") , "", file_get_contents($_POST["testurl"]);
+	echo $textForm;
+    	/*require_once("simplifier.php");
     	$texteSimplifie = simplifierTexteBrut($textForm,2);
-	echo str_replace("\n","<br />",$texteSimplifie);
+	echo str_replace("\n","<br />",$texteSimplifie);*/
 	echo '</p>';
     ?>
 
