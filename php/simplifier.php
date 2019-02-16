1707217c5750b384b7996585e006531b006db854
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-02-16 19:28:36 +0100

    ajout fonction chercherMotBDD

--- php/simplifier.php
+++ php/simplifier.php
@@ -1,5 +1,50 @@
 <?php
   require('Bdd.php');
+  
+  function chercherMotBDD($mot, &$bdd, &$numModal)
+  {
+    $texteRetour = "";
+    $sql = "SELECT DEFINITION FROM TABLE_DEFINITION WHERE MOT LIKE '$mot' AND CLASSEMENT = 1;";
+    $res = $bdd->query($sql); //On récupère (s'il en existe) les lignes de notre table "produits" qui répondent à notre requête $sql.
+                  //Ces lignes sont stockées dans la variables $res qui est un tableau du jeu de résultat de notre requête.
+    if(!empty($row = $res->fetch())) //La méthode fetch() permet de récupérer la ligne suivante du résultat de notre requete qui sont stockés dans la variable $res.
+    {
+        //$texteSimplifie .= '<span class="vocabulaire"><span class="expression">'.$mot.'</span><span style="display:none" class="definition hidden">'.$row['DEFINITION'].'</span></span>';
+        $texteRetour .= '
+        <span class="vocabulaireSpecifique">
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalNb'.$numModal.'">
+            '.$mot.'
+          </button>
+
+          <!-- Modal -->
+          <div class="modal fade" id="modalNb'.$numModal.'" tabindex="-1" role="dialog" aria-labelledby="modalNb'.$numModal.'Label" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="modalNb'.$numModal.'Label">'.$mot.'</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  '.$row['DEFINITION'].'
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </span>';
+        $numModal += 1;
+    }
+    else
+    {
+      $texteRetour .= "$mot";            
+    }
+    return $texteRetour;
+  }
 
   //$balise =
   //0: on cherche des mots difficiles dans les balises (<>),
@@ -101,17 +146,7 @@
     }
     if(strlen($mot) > 0)
         {
-          $sql = "SELECT DEFINITION FROM TABLE_DEFINITION WHERE MOT LIKE '$mot' AND CLASSEMENT = 1;";
-      		$res = $bdd->query($sql); //On récupère (s'il en existe) les lignes de notre table "produits" qui répondent à notre requête $sql.
-      								  //Ces lignes sont stockées dans la variables $res qui est un tableau du jeu de résultat de notre requête.
-      		if(!empty($row = $res->fetch())) //La méthode fetch() permet de récupérer la ligne suivante du résultat de notre requete qui sont stockés dans la variable $res.
-      		{
-      				//$texteSimplifie .= '<span class="vocabulaire"><span class="expression">'.$mot.'</span><span style="display:none" class="definition hidden">'.$row['DEFINITION'].'</span></span>';
-      		}
-      		else
-      		{
-      			$texteSimplifie .= "$mot";
-      		}
+          $texteSimplifie .= chercherMotBDD($mot, $bdd, $numModal);
         }
     
     return $texteSimplifie;
