e8d44df4c8d0d6558877e8cb2d6c386e8d2d0859
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-02-16 19:04:57 +0100

    modal verticalement centré

--- php/simplifier.php
+++ php/simplifier.php
@@ -53,7 +53,7 @@
 
                 <!-- Modal -->
                 <div class="modal fade" id="modalNb'.$numModal.'" tabindex="-1" role="dialog" aria-labelledby="modalNb'.$numModal.'Label" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
+                  <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                       <div class="modal-header">
                         <h5 class="modal-title" id="modalNb'.$numModal.'Label">'.$mot.'</h5>
