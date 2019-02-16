362cea75df723f3fe0f1326713fac8c6e2cdbb9d
SlaoutiYannis <44498934+SlaoutiYannis@users.noreply.github.com>
2019-02-17 00:23:56 +0100

    trying

--- php/dictionnaire_data.php
+++ php/dictionnaire_data.php
@@ -5,7 +5,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "BDDTradocteur";
-$dbname = "bdd_tradocteur";
+$dbname = "BDD_TRADOCTEUR";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 
