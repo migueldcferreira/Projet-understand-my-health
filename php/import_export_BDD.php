55a47b4f158195958012963ad77e39077430620b
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-04 17:18:07 +0100

    Update import_export_BDD.php

--- php/import_export_BDD.php
+++ php/import_export_BDD.php
@@ -17,8 +17,8 @@
                     <div class="col-md-12">
                         <nav>
                             <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-                                <a class="nav-item nav-link active" id="nav-import-tab" data-toggle="tab" href="#nav-import" role="tab" aria-controls="nav-import" aria-selected="true"><i class="fas fa-file-pdf"></i>Importez des définitions</a>
-                                <a class="nav-item nav-link" id="nav-export-tab" data-toggle="tab" href="#nav-export" role="tab" aria-controls="nav-export" aria-selected="false"><i class="fas fa-code"></i>Exportez des définitions</a>
+                                <a class="nav-item nav-link active" id="nav-import-tab" data-toggle="tab" href="#nav-import" role="tab" aria-controls="nav-import" aria-selected="true"><i class="fas fa-file-upload"></i> Importez des définitions</a>
+                                <a class="nav-item nav-link" id="nav-export-tab" data-toggle="tab" href="#nav-export" role="tab" aria-controls="nav-export" aria-selected="false"><i class="fas fa-file-csv"></i> Exportez des définitions</a>
                             </div>
                         </nav>
                         <div class="tab-content" id="nav-tabContent">
@@ -27,28 +27,19 @@
                               <div class="container">
                                 <div class="panel panel-default">
                                   <div class="panel-heading"><strong>Sélectionnez des fichiers dans votre ordinateur</strong></div>
-                                  <div class="panel-body">
-
-									  <!-- Standar Form -->
-									
+                                  <div class="panel-body">									
                                     <form action="simplifierByUpload.php" method="post" enctype="multipart/form-data" id="js-upload-form">
-                                    <div class="form-inline">
-                                      <div class="form-group">
-                                        <input type="file" name="fichier" id="js-upload-files" multiple aria-describedby="fileHelp" accept=".txt,.pdf">
+                                      <div class="form-inline">
+                                        <div class="form-group">
+                                          <input type="file" name="fichier" id="js-upload-files" multiple aria-describedby="fileHelp" accept=".txt,.csv">
+                                        </div>
+                                        <button type="submit" class="btn btn-sm btn-primary" id="js-upload-submit">Importer</button>
                                       </div>
-                                      <button type="submit" class="btn btn-sm btn-primary" id="js-upload-submit">Simplifier</button>
-                                    </div>
                                     </form>
-
-									  <!-- Drop Zone -->
-                                    <h4>Ou glisser-déposer ici</h4>
-                                    <div class="upload-drop-zone" id="drop-zone">
-                                    Glisser-déposer ici
                                   </div>
                                 </div>
-                              </div>
-                            </div> <!-- /container -->					
-                          </div>
+                              </div> <!-- /container -->					
+                            </div>
                           
                           
                           <div class="tab-pane fade" id="nav-export" role="tabpanel" aria-labelledby="nav-export-tab">
