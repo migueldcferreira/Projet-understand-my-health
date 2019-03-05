1b728d376fd33707437c0b58c677f3cb011bf45d
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-05 18:14:55 +0100

    Update importerBDD.php

--- php/importerBDD.php
+++ php/importerBDD.php
@@ -9,9 +9,41 @@
 </head>
 
 <body>
-	<?php include("menu_admin.php"); ?>
+	<?php 
+		include("menu_admin.php");
+		require('Bdd.php');
 
+		//verification de l'extension du fichier en entre
+		$extension = substr($_FILES['fichier']['name'], -3, 3);
+		if ($extension == 'txt' OR $extension == 'csv' OR $extension == 'sql') 
+		{
+			$textImport = strtok(file_get_contents($_FILES["fichier"]["tmp_name"]), "\n");
+		}
+		else
+		{
+			die("Veuillez insérer un fichier avec une extension valide (.txt, .csv, .sql");
+		}
 
-  <?php include("script_menu.php"); ?>
+		//connexion a la base de donnees   
+    try
+    {
+      $bdd = Bdd::connect("BDD_TRADOCTEUR");
+      $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      //Permet de récuperer une exception lorsque il y a une erreur au niveau de la base de donnée.
+      //On pourra donc traiter l'erreur plus simplement avec un try et catch.
+    }
+    catch (Exception $e)
+    {
+    	die('Erreur : ' . $e->getMessage());
+    }
+	
+		while ($textImport !== false)
+		{
+    	echo "Ligne to add= $textImport<br />";
+    	$textImport = strtok("\n");
+		}
+
+		php include("script_menu.php");
+	?>
 </body>
 </html>
