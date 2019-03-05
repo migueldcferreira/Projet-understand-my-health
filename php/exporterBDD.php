c7002f508aebe4cf5e3434b984436ac7cceab80c
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-05 16:05:31 +0100

    debuggage

--- php/exporterBDD.php
+++ php/exporterBDD.php
@@ -57,14 +57,14 @@
     
     fclose($myfile);
     
-    header('Content-Description: File Transfer');
+    /*header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename="'.basename($file).'"');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Content-Length: ' . filesize($file));
-    readfile($file);
+    readfile($file);*/
   ?>
 
 </body>
