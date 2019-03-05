6bafac66a6ff8417d63e7a09905c394030c8866b
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-05 17:52:22 +0100

    Create importerBDD.php

---
+++ php/importerBDD.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<?php session_start();
+	include ('verif_admin.php'); 
+	include("head.php"); ?>
+  <link rel="stylesheet" href="..\css/choosetrad.css">
+</head>
+
+<body>
+	<?php include("menu_admin.php"); ?>
+
+
+  <?php include("script_menu.php"); ?>
+</body>
+</html>
