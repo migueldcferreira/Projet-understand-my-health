9a2091c42e9be124be253cc08aa449608ee64f72
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-08 21:38:06 +0100

    incrémentation du compteur de def acceptée

--- php/ajouter.php
+++ php/ajouter.php
@@ -16,15 +16,25 @@
 
 //Promouvoir
 $id = isset($_GET['id']) ? (int) $_GET['id'] : null;
-if ($id) {
-
-
-	// sql to delete a record
-		$sql = 'UPDATE TABLE_DEFINITION SET A_CONFIRMER = 0 WHERE ID_DEFINITION= "'.$id.'"';
-		$stmt= $bdd->query($sql);
-		$stmt->execute($sql);
-    header('location: admin_liste_def.php');
-
+if ($id)
+{
+	//passage d'une definition a confirmer -> definition acceptee
+	$sql = "UPDATE TABLE_DEFINITION SET A_CONFIRMER = 0 WHERE ID_DEFINITION= ".$id.";";
+	$stmt= $bdd->query($sql);
+	$stmt->execute($sql);
+	
+	//on cherche quel est l'id de l'utilisateur ayant soumis la definition
+	$sql = "SELECT ID_UTILISATEUR_MODIF FROM TABLE_DEFINITION WHERE ID_DEFINITION=".$id.";";
+	$res = $bdd->query($sql);
+	$row = $res->fetch();
+	$idU = $row['ID_UTILISATEUR_MODIF'];
+	
+	//on augmente de 1 son nombre de definition acceptee
+	$sql = "UPDATE TABLE_UTILISATEUR SET NB_DEF_ACCEPTEE = NB_DEF_ACCEPTEE+1 WHERE ID_UTILISATEUR=".$idU.";";
+	$res = $bdd->query($sql);
+	
+	header('location: admin_liste_def.php');
 }
+
 else{header('location: register.php'); }
 ?>
