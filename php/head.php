d0462aeb4423a34e8e1dcd249418345a72535476
hugomartinshm <44262443+hugomartinshm@users.noreply.github.com>
2019-03-10 21:19:39 +0100

    retour au bootstrap de base

--- php/head.php
+++ php/head.php
@@ -4,9 +4,9 @@
 
 <!-- Bootstrap CSS -->
 <!-- Ci dessous le bootstrap de base-->
-<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <!-- Ci dessous le bootstrap (fichier css) contenu dans GitHub-->
-<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+<!--<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">-->
 
 <!--Font Awesome CSS-->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
