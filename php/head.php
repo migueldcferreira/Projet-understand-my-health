3f8b4b91ed8170f59d891d0f95792779a1a33b6e
hugomartinshm <44262443+hugomartinshm@users.noreply.github.com>
2019-03-10 20:32:12 +0100

    Modification du boostrap (test)

--- php/head.php
+++ php/head.php
@@ -3,8 +3,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<!--<link rel="stylesheet" type="text/css" href="css/bootstrap.css">-->
+<!-- Ci dessous le bootstrap de base-->
+<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">-->
+<!-- Ci dessous le bootstrap (fichier css) contenu dans GitHub-->
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
 
 <!--Font Awesome CSS-->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
