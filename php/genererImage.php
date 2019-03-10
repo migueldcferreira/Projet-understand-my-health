23d2b08b0339ec7a095fc669a43861dd93a33e22
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-10 18:29:02 +0100

    Update genererImage.php

--- php/genererImage.php
+++ php/genererImage.php
@@ -8,23 +8,25 @@
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //Permet de récuperer une exception lorsque il y a une erreur au niveau de la base de donnée.
     //On pourra donc traiter l'erreur plus simplement avec un try et catch.
+    
+    //on verifie que l'id a bien ete donne
+    if(isset($_GET['id']))
+    {
+      $idImage = $_GET['id'];
+
+      //on cherche l'image dans la bdd
+      $sdl = "SELECT IMAGE FROM TABLE_IMAGE WHERE ID_IMAGE = ".$idImage.";"; 
+      $res = $bdd->query($sdl); 
+      $row = $res->fetch(); 
+
+      header('Content-Type: image');
+      echo $row['IMAGE'];
+    }
   }
   catch (Exception $e)
   {
-    die("Erreur connexion base de données);
+    die('Erreur : ' . $e->getMessage());
   }
   
-  //on verifie que l'id a bien ete donne
-  if(isset($_GET['id']))
-  {
-    $idImage = $_GET['id'];
   
-    //on cherche l'image dans la bdd
-    $sdl = "SELECT IMAGE FROM TABLE_IMAGE WHERE ID_IMAGE = ".$idImage.";"; 
-    $res = $bdd->query($sdl); 
-    $row = $res->fetch(); 
-
-    header('Content-Type: image');
-    echo $row['IMAGE'];
-  }
 ?>
