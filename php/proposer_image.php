0a072da2d2756805f205d91875cbdbde2a3a35e4
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-10 16:43:26 +0100

    Update proposer_image.php

--- php/proposer_image.php
+++ php/proposer_image.php
@@ -1,41 +1,40 @@
 <!doctype html>
 <html lang="fr">
-  <head>
-    <?php
+<head>
+	<?php
 		include('Bdd_inscription.php');
 		include('verif_membre.php');
 		include("head.php");
 	?>
 
-    <link rel="stylesheet" href="..\css/choosetrad.css">
-  </head>
-  <body>
+	<link rel="stylesheet" href="..\css/choosetrad.css">
+</head>
+	
+<body>
 	<?php include("menu.php"); ?>
 	<div class="header form_head bg-success">
-  		<h2> Proposer l'ajout d'une nouvelle définition</h2>
+		<h2> Proposer l'ajout d'une nouvelle image</h2>
 	</div>
 
-	<form method="post" action="proposer_definition_mot.php" class = "formulaire_stylise">
-  	<?php include('errors.php');?>
-  	<div class="input-group">
-  		<label>Mot : </label>
-  		<input type="text" name="MOT" >
-  	</div>
-  	<div class="form-group">
-  		<label>Definition : </label>
-  		<textarea class="form-control" type="text" name="DEFINITION" rows="3"></textarea>
-  	</div>
-    <div class="input-group">
-      <input type="hidden" name="username" value= "<?php echo $_SESSION['username']; ?>">
-    </div>
-  	<div class="input-group form-group">
-  		<button type="submit" class="btn btn-success btn-sm"  name="proposerDef">Proposer</button>
-  	</div>
+	<form enctype="multipart/form-data" method="post" action="proposer_definition_mot.php" class = "formulaire_stylise">
+		<?php include('errors.php');?>
+		<div class="input-group">
+			<label>Mot : </label>
+			<input type="text" name="MOT" >
+		</div>
+		<div class="form-group">
+			<label>Image : </label>
+			<input type="hidden" name="MAX_FILE_SIZE" value="250000" />
+			<input type="file" name="IMAGE" id="js-upload-files" multiple aria-describedby="fileHelp" accept=".png,.jpg,.jpeg">
+		</div>
+		<div class="input-group form-group">
+			<button type="submit" class="btn btn-success btn-sm"  name="proposerDef">Proposer</button>
+		</div>
 
 
 
 
 	<?php include("script_menu.php"); ?>
 
-  </body>
+</body>
 </html>
