da63ddf38dc60e097c741bac584e886a09eaa712
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-17 00:19:11 +0100

    Update proposer_image.php

--- php/proposer_image.php
+++ php/proposer_image.php
@@ -25,7 +25,7 @@
 			//on verifie que le champ du mot n'est pas vide
 			$mot = $_POST['MOT'];
 			$liste_mot = $_POST['LISTE_MOT'];
-			echo $liste_mot;
+			echo gettype($liste_mot);
 			$liste_mot = preg_replace(" ," , ",", $liste_mot);
 			echo $liste_mot;
 			$liste_mot = explode(",", $liste_mot);
