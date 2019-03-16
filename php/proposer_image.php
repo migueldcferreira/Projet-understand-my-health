d3ebc9171450e9556f58ee2dde6fcb792a0deb0c
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-16 22:05:37 +0100

    Modif pour correspondre a TABLE_LIEN_MOT_IMAGE

--- php/proposer_image.php
+++ php/proposer_image.php
@@ -21,6 +21,11 @@
 		//si le membre a appuye sur le bouton "proposer"
 		if(isset($_POST['proposerImage']))
 		{
+			if(empty($mot))
+			{
+				array_push($errors, "Veuillez entrer un mot");
+			}
+			
 			//on verifie si on a bien recupere l'image
 			$verifImage = false;
 			$verifImage = is_uploaded_file($_FILES['IMAGE']['tmp_name']);
@@ -38,11 +43,7 @@
 			
 			$image = addslashes(file_get_contents($_FILES['IMAGE']['tmp_name']));
 			$type = $_FILES['IMAGE']['type'];
-			$mot = $_POST['MOT'];			
-			if(empty($mot))
-			{
-				array_push($errors, "Veuillez entrer un mot");
-			}
+			$mot = $_POST['MOT'];						
 			
 			//si il n'y a pas eu d'erreur dans le remplissage du formulaire
 			if(count($errors) == 0)
@@ -75,17 +76,34 @@
 					$sql = "UPDATE TABLE_UTILISATEUR SET NB_DEF_ACCEPTEE = NB_DEF_ACCEPTEE+1 WHERE ID_UTILISATEUR=".$id.";";
 					$res = $bdd->query($sql);
 				}
+			
+				//on determine l'id de l'image
+				$sql = "SELECT COALESCE(MIN(ID_IMAGE)+1,1) AS ID FROM TABLE_IMAGE WHERE ID_IMAGE+1 NOT IN (SELECT ID_IMAGE FROM TABLE_IMAGE);"
+				$res = $bdd->query($sql);
+				$row = $res->fetch();
+				$id_image = $row['ID'];
 				
-				//on determine le classement de la definition selon sa taille
-				$sql = "SELECT COALESCE(MAX(CLASSEMENT),0) AS CLA FROM TABLE_IMAGE WHERE MOT='".$mot."';";
+				//on ajoute l'image a la bdd
+				$sql = "INSERT INTO TABLE_IMAGE (ID_IMAGE, IMAGE, TAILLE, TYPE, ID_UTILISATEUR_MODIF, A_CONFIRMER) VALUES (".$id_image.", '".$image."', ".$taille.", '".$type."', ".$id.", ".$confirmation.") ;";
+				$res = $bdd->query($sql);
+				
+				//a faire pour chaque mot defini par l'image
+				
+				//on determine le classement selon le nombre d'image deja presente pour definir ce mot
+				$sql = "SELECT COALESCE(MAX(CLASSEMENT),0) AS CLA FROM TABLE_LIEN_MOT_IMAGE WHERE MOT='".$mot."';";
 				$res = $bdd->query($sql);
 				$row = $res->fetch();
 				$classement = $row['CLA']+1;
-				//echo "Id utilisateur : $id, Classement : $classement, A_CONFIRMER : $confirmation, Image: $image";
 				
-				//on ajoute l'image a la bdd
-				$sql = "INSERT INTO TABLE_IMAGE (MOT, IMAGE, TAILLE, TYPE, ID_UTILISATEUR_MODIF, CLASSEMENT, A_CONFIRMER) VALUES ('".$mot."' ,'".$image."', ".$taille.", '".$type."', ".$id.", ".$classement.", ".$confirmation.") ;";
-				$res = $bdd->query($sql);		
+				//on determine l'id du lien
+				$sql = "SELECT COALESCE(MIN(ID_LIEN)+1,1) AS ID FROM TABLE_LIEN_MOT_IMAGE WHERE ID_LIEN+1 NOT IN (SELECT ID_LIEN FROM TABLE_LIEN_MOT_IMAGE);"
+				$res = $bdd->query($sql);
+				$row = $res->fetch();
+				$id_lien = $row['ID'];
+				
+				//on ajoute le lien entre l'image et les mots saisies
+				$sql = "INSERT INTO TABLE_LIEN_MOT_IMAGE (ID_LIEN, MOT, ID_IMAGE, CLASSEMENT) VALUES (".$id_lien.", '".$mot."' ,".$id_image.", ".$classement.") ;";
+				$res = $bdd->query($sql);
 			}
 		}
 	?>
