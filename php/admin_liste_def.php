9f6199989acb23dfd2587b45b16485a3cda6b610
SlaoutiYannis <44498934+SlaoutiYannis@users.noreply.github.com>
2019-03-17 20:59:25 +0100

    DATATABLES FTW

--- php/admin_liste_def.php
+++ php/admin_liste_def.php
@@ -1,11 +1,130 @@
 <!doctype html>
 <html lang="fr">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	 <?php session_start();
-	 include ('verif_admin.php'); 
+	<?php include("script_menu.php"); 
+	session_start();
+	 include ("verif_admin.php"); 
 	include("head.php"); ?>
-    <link rel="stylesheet" href="..\css/choosetrad.css">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+	<link rel="stylesheet" href="..\css/choosetrad.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+   
+    <script type="text/javascript" language="javascript" >
+			$(document).ready(function() {
+				var dataTable = $('#admin_def_1').DataTable( {
+					"processing": true,
+					"serverSide": true,
+					"order": [[ 2, "desc" ]],
+					"columnDefs": [ {
+					"targets": 3,
+					"orderable": false
+					} ],
+					"language": { 
+
+					    "sProcessing": "Traitement en cours ...",
+					    "sLengthMenu": "Afficher _MENU_ lignes",
+					    "sZeroRecords": "Aucun résultat trouvé",
+					    "sEmptyTable": "Aucune donnée disponible",
+					    "sInfo": "Lignes _START_ à _END_ sur _TOTAL_",
+					    "sInfoEmpty": "Aucune ligne affichée",
+					    "sInfoFiltered": "(Filtrer un maximum de_MAX_)",
+					    "sInfoPostFix": "",
+					    "sSearch": "Chercher:",
+					    "sUrl": "",
+					    "sInfoThousands": ",",
+					    "sLoadingRecords": "Chargement...",
+					    "oPaginate": {
+					      "sFirst": "Premier", "sLast": "Dernier", "sNext": "Suivant", "sPrevious": "Précédent"
+					    },
+					    "oAria": {
+					      "sSortAscending": ": Trier par ordre croissant", "sSortDescending": ": Trier par ordre décroissant"
+					    }
+					} ,
+
+					"ajax":{
+						url :"admin_def_data_1.php", // json datasource
+						type: "post",  // method  , by default get
+						dataFilter: function(reps) {
+				                console.log(reps);
+				                return reps;
+		           			 },
+		           		error:function(err){
+			                  console.log(err);
+			            },
+						error: function(){  // error handling
+							$(".admin_def_1-error").html("");
+							$("#admin_def_1").append('<tbody class="admin_def_1-error"><tr><th colspan="3">Pas de données trouvées sur le serveur</th></tr></tbody>');
+							$("#admin_def_1_processing").css("display","none");
+							
+						}
+					}
+				} );
+
+			} );
+
+
+			$(document).ready(function() {
+				var dataTable = $('#admin_def_2').DataTable( {
+					"processing": true,
+					"serverSide": true,
+					"order": [[ 2, "desc" ]],
+					"columnDefs": [ {
+					"targets": 3,
+					"orderable": false
+					} ],
+					"language": { 
+
+					    "sProcessing": "Traitement en cours ...",
+					    "sLengthMenu": "Afficher _MENU_ lignes",
+					    "sZeroRecords": "Aucun résultat trouvé",
+					    "sEmptyTable": "Aucune donnée disponible",
+					    "sInfo": "Lignes _START_ à _END_ sur _TOTAL_",
+					    "sInfoEmpty": "Aucune ligne affichée",
+					    "sInfoFiltered": "(Filtrer un maximum de_MAX_)",
+					    "sInfoPostFix": "",
+					    "sSearch": "Chercher:",
+					    "sUrl": "",
+					    "sInfoThousands": ",",
+					    "sLoadingRecords": "Chargement...",
+					    "oPaginate": {
+					      "sFirst": "Premier", "sLast": "Dernier", "sNext": "Suivant", "sPrevious": "Précédent"
+					    },
+					    "oAria": {
+					      "sSortAscending": ": Trier par ordre croissant", "sSortDescending": ": Trier par ordre décroissant"
+					    }
+					} ,
+
+					"ajax":{
+						url :"admin_def_data_2.php", // json datasource
+						type: "post",  // method  , by default get
+						dataFilter: function(reps) {
+				                console.log(reps);
+				                return reps;
+		           			 },
+		           		error:function(err){
+			                  console.log(err);
+			            },
+						error: function(){  // error handling
+							$(".admin_def_2-error").html("");
+							$("#admin_def_2").append('<tbody class="admin_def_2-error"><tr><th colspan="3">Pas de données trouvées sur le serveur</th></tr></tbody>');
+							$("#admin_def_2_processing").css("display","none");
+							
+						}
+					}
+				} );
+
+			} );
+
+
+				
+
+		
+		</script>
+
+   
 
 </head>
 <body>
@@ -13,69 +132,34 @@
 	<?php include("menu_admin.php"); ?>
 
 
-<?php
 
 
-	require('Bdd.php');
-
-	try
-	{
-		$bdd = Bdd::connect("BDD_TRADOCTEUR");
-		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Permet de récuperer une exception lorsque il y a une erreur au niveau de la base de donnée.
-																	   //On pourra donc traiter l'erreur plus simplement avec un try et catch.
-		$sql_base = "SELECT ID_DEFINITION, MOT, DEFINITION, DATE_MODIF FROM TABLE_DEFINITION WHERE A_CONFIRMER = 0"; //requête pour trouver les définitions validées
-		$res_base = $bdd->query($sql_base);
-
-		$sql_prop = "SELECT ID_DEFINITION, MOT, DEFINITION, DATE_MODIF FROM TABLE_DEFINITION WHERE A_CONFIRMER = 1"; //requête pour trouver les définitions non validées (propositions)
-		$res_prop = $bdd->query($sql_prop);
-
-	}
-	catch (Exception $e)
-	{
-			die('Erreur : ' . $e->getMessage());
-	}?>
-
-
-<section id="tabs" class="project-tab">
+		<section id="tabs" class="project-tab">
                         <nav>
                             <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
                                 <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true"><i class="fa fa-users"></i> Propositions de définitions</a>
                                 <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false"><i class="fa fa-database"></i> Base de définitions</a>
                             </div>
                         </nav>
-                        <div class="tab-content" id="nav-tabContent">
+                      <div class="tab-content" id="nav-tabContent">
                             <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-															<div class="row">
-																<div class="col-md-12 table-responsive-sm">
-																	<table class="table">
-																		<thead>
-																			<tr>
-																				<th scope="col">Mot</th>
-																				<th scope="col">Définition</th>
-																				<th scope="col">Date_ajout</th>
-																				<th scope="col">Utilisateur</th>
-																				<th scope="col"></th>
-																			</tr>
-																		</thead>
-																		<tbody>
-																			<?php while ($row = $res_prop->fetch()) { ?>
-																				<tr>
-																						<td> <?php printf ("%s", $row[1]); ?> </td>
-																						<td> <?php printf ("%s", $row[2]); ?> </td>
-																						<td> <?php printf ("%s", $row[3]); ?> </td>
+														</br>
 
-																								<td>
-																						<a href="ajouter.php?id=<?php echo $row[0] ?>"> <button class="btn btn-success btn-sm tooltipsAdmin" title="Ajouter la définition"><i class="fas fa-check"></i></button>
-																						<a href="modifier.php?id=<?php echo $row[0] ?>">   <button class="btn btn-sm tooltipsAdmin enabled" title="Modifier cette définition"><i class="fa fa-edit"></i></button>      </a>
-																						<a href="supprimer_def.php?id=<?php echo $row[0] ?>"> <button class="btn btn-danger btn-sm tooltipsAdmin enabled" title="Supprimer cette définition"><i class="fas fa-minus-circle"></i></button> </a>
-																						</td>
+															<div class="container">
+																	<table id="admin_def_1"  cellpadding="0" cellspacing="0" border="0" class="display" width="100%">
+																			<thead>
+																				<tr>
+																					<th>mot</th>
+																					<th>definition</th>
+																					<th>date_modif </th>
+																					<th>Actions</th>
 																				</tr>
-																			<?php } ?>
-																		</tbody>
+																			</thead>
 																	</table>
-																</div>
 															</div>
 
+
+														
 
 
 
@@ -84,52 +168,28 @@
                             </div>
                             <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
 
+                            							</br>
+														
 
-															<div class="row">
-																<div class="col-md-12 table-responsive-sm">
-																	<table class="table">
-																		<thead>
-																			<tr>
-																				<th scope="col">Mot</th>
-																				<th scope="col">Définition</th>
-																				<th scope="col">Date_ajout</th>
-																				<th scope="col"></th>
-																			</tr>
-																		</thead>
-																		<tbody>
-																			<?php while ($row = $res_base->fetch()) { ?>
+                            								<div class="container">
+																	<table id="admin_def_2"  cellpadding="0" cellspacing="0" border="0" class="display" width="100%">
+																			<thead>
 																				<tr>
-																						<td> <?php printf ("%s", $row[1]); ?> </td>
-																						<td> <?php printf ("%s", $row[2]); ?> </td>
-																						<td> <?php printf ("%s", $row[3]); ?> </td>
-
-					        															<td>
-																						<a href="modifier.php?id=<?php echo $row[0] ?>">   <button class="btn btn-sm tooltipsAdmin enabled" title="Modifier cette définition"><i class="fa fa-edit"></i></button>      </a>
-																						<a href="supprimer_def.php?id=<?php echo $row[0] ?>"> <button class="btn btn-danger btn-sm tooltipsAdmin enabled" title="Supprimer cette définition"><i class="fas fa-minus-circle"></i></button> </a>
-																						</td>
+																					<th>Mot</th>
+																					<th>Définition</th>
+																					<th>Date d'ajout</th>
+																					<th>Actions</th>
 																				</tr>
-																			<?php } ?>
-																		</tbody>
+																			</thead>
 																	</table>
-																</div>
 															</div>
-
-
-
-
-
 
                             </div>
 
                         </div>
     </section>
+		
+	</body>
 
-
-
-
-<?php include("script_menu.php"); ?>
-
-<script type="text/javascript" src="..\javascript/tooltipsAdmin.js"></script>
-
-</body>
 </html>
+
