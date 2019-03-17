981a826c452266c4c82d8541b2a82b1cbeca34bc
hugomartinshm <44262443+hugomartinshm@users.noreply.github.com>
2019-03-17 13:40:26 +0100

    passage en blockquote

--- php/simplifierByURL.php
+++ php/simplifierByURL.php
@@ -47,7 +47,9 @@
 	  <input class="btn btn-secondary" type="submit" name="simplifier" value="Export PDF">
 	</form>
 	<?php
+	echo '<blockquote class="blockquote mx-4 my-3 text-justify" style="line-height:200%"><p>';
 	echo $texteSimplifie;
+	echo '</p></blockquote>';
     ?>
 
    
