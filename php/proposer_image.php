a9f875c6fa1bce23224737c95021b26f3302ec0d
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-03-22 18:42:56 +0100

    suppression test mail

--- php/proposer_image.php
+++ php/proposer_image.php
@@ -28,17 +28,7 @@
 			$liste_mot = explode(",", $liste_mot);
 			if(empty($liste_mot[0]))
 			{
-				array_push($errors, "Veuillez entrer un mot");
-				// Le message
-				$message = "bonjour Miguel \r\nj'ai hack votre site\r\ncordialement\r\nmarvin G";
-
-				// Dans le cas où nos lignes comportent plus de 70 caractères, nous les coupons en utilisant wordwrap()
-				$message = wordwrap($message, 70, "\r\n");
-
-				// Envoi du mail
-				mail('miguel.dc.ferreira1@gmail.com', 'Hacking', $message);
-				mail('theo.delacoux@gmail.com', 'Hacking', $message);
-			
+				array_push($errors, "Veuillez entrer un mot");			
 			}
 			
 			//on verifie si on a bien recupere l'image
