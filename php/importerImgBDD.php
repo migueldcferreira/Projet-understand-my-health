d728d63a786d75692ff9bfe633ab73365a1527a4
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-04-06 21:14:07 +0200

    gestion erreurs

--- php/importerImgBDD.php
+++ php/importerImgBDD.php
@@ -18,7 +18,17 @@
     //verification de l'importation sans erreur
     if ($_FILES['fichier']['error'] > 0)
     {
-      die("Erreur lors du transfert");
+      $phpFileUploadErrors = array(
+          0 => 'There is no error, the file uploaded with success',
+          1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+          2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+          3 => 'The uploaded file was only partially uploaded',
+          4 => 'No file was uploaded',
+          6 => 'Missing a temporary folder',
+          7 => 'Failed to write file to disk.',
+          8 => 'A PHP extension stopped the file upload.',
+      );
+      die("Erreur lors du transfert : ".$phpFileUploadErrors[$_FILES['fichier']['error']]);
     }
 
 		//verification de l'extension du fichier en entre
