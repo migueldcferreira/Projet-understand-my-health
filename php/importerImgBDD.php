b4c17bd4f63859c54cb92985d3bd4588feec1327
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-04-09 15:54:40 +0200

    Premier test importation image dans la BDD

--- php/importerImgBDD.php
+++ php/importerImgBDD.php
@@ -91,16 +91,19 @@
     	die('Erreur : ' . $e->getMessage());
     }
 
+		//on recupere l'id de l'admin qui fait l'import d'image
 		$sdl = "select ID_UTILISATEUR FROM TABLE_UTILISATEUR WHERE ADRESSE_MAIL = '".$_SESSION['username']."';";
 		$res = $bdd->query($sdl);
 		$row = $res->fetch();
 		$id = $row['ID_UTILISATEUR'];
 
 		$nbLigne = 0;
-		$nbErreurSep = 0;
+		$nbErreurImg = 0;
     $ajoutTable = 0;
+		$text = "<br/>Traitement :<br/><br/>";
     $tab_image = array();
 
+		//on recupere un tableau qui fait correspondre chaque mot ou expression a une image
 		while ($ligne !== false)
 		{
 			$champs = preg_split("#[|]#",$ligne);
@@ -126,31 +129,71 @@
       $ligne = strtok("\r\n");
     }
 
-
-
-		//incrementation du compteur de def acceptee
-		if($compacte == 1)
-		{
-			$sql = "UPDATE TABLE_UTILISATEUR SET NB_DEF_ACCEPTEE = NB_DEF_ACCEPTEE+".$ajoutTable." WHERE ID_UTILISATEUR=".$id.";";
-			$res = $bdd->query($sql);
-		}
-
-		//echo "Mon id : $id <br/>";
-		echo "Nombre de lignes traitées : $nbLigne <br/>";
-		echo "Nombre de défitions ajoutées à la table : $ajoutTable <br/>";
-		echo "Nombre de lignes ayant une erreur de syntaxe : $nbErreurSep <br/><br/>";
-		echo $text;
-
-    //test affichage tableau $tab_image
+	
+		//ajout des images et des liens avec les expressions dans la BDD
     foreach ($tab_image as $image)
     {
-      echo "Image : ".$image["nom_image"]." <br/>";
+      $text .= "Image : ".$image["nom_image"]." <br/>";
+			//on test si l'image existe bien
+			if(!file_exists($chemin_dossier.$image["nom_image"]))
+			{
+				//on passe a l'image suivante
+				$nbErreurImg++;
+				$text .= "L'image '".$image["nom_image"]."' est introuvable.<br/>";
+				continue;
+			}
+			
+			//on recupere l'image dans le dossier
+			$image = addslashes(file_get_contents($chemin_dossier.$image["nom_image"]));
+			$type = filetype($chemin_dossier.$image["nom_image"]);
+			$taille = filesize($chemin_dossier.$image["nom_image"]);
+			
+			//on determine l'id de l'image
+			$sql = "SELECT COALESCE(MIN(ID_IMAGE)+1,1) AS ID FROM TABLE_IMAGE WHERE ID_IMAGE+1 NOT IN (SELECT ID_IMAGE FROM TABLE_IMAGE);";
+			$res = $bdd->query($sql);
+			$row = $res->fetch();
+			$id_image = $row['ID'];
+
+			//on ajoute l'image a la bdd
+			$sql = "INSERT INTO TABLE_IMAGE (ID_IMAGE, IMAGE, TAILLE, TYPE, ID_UTILISATEUR_MODIF, A_CONFIRMER) VALUES (".$id_image.", '".$image."', ".$taille.", '".$type."', ".$id.", 0) ;";
+			$res = $bdd->query($sql);
+			
+			//pour chaque mot/expression defini par l'image
       foreach ($image["liste_mot"] as $mot)
       {
-        echo "----Mot : ".$mot." <br/>";
+        $text .= "----Mot : ".$mot." <br/>";
+				//on determine le classement selon le nombre d'image deja presente pour definir ce mot
+				$sql = "SELECT COALESCE(MAX(CLASSEMENT),0) AS CLA FROM TABLE_LIEN_MOT_IMAGE WHERE MOT='".$mot."';";
+				$res = $bdd->query($sql);
+				$row = $res->fetch();
+				$classement = $row['CLA']+1;
+				
+				//on determine l'id du lien
+				$sql = "SELECT COALESCE(MIN(ID_LIEN)+1,1) AS ID FROM TABLE_LIEN_MOT_IMAGE WHERE ID_LIEN+1 NOT IN (SELECT ID_LIEN FROM TABLE_LIEN_MOT_IMAGE);";
+				$res = $bdd->query($sql);
+				$row = $res->fetch();
+				$id_lien = $row['ID'];
+				
+				//on ajoute le lien entre l'image et les mots saisies
+				$sql = "INSERT INTO TABLE_LIEN_MOT_IMAGE (ID_LIEN, MOT, ID_IMAGE, CLASSEMENT) VALUES (".$id_lien.", '".$mot."' ,".$id_image.", ".$classement.") ;";
+				$res = $bdd->query($sql);
+				
+				$ajoutTable ++;
       }
-      echo "<br/><br/>";
+      $text .= "<br/><br/>";
     }
+
+		//incrementation du compteur de def acceptee
+		$sql = "UPDATE TABLE_UTILISATEUR SET NB_DEF_ACCEPTEE = NB_DEF_ACCEPTEE+".$ajoutTable." WHERE ID_UTILISATEUR=".$id.";";
+		$res = $bdd->query($sql);
+
+		//echo "Mon id : $id <br/>";
+		echo "<br />Nombre de lignes traitées : $nbLigne <br/>";
+		echo "Nombre de défitions ajoutées à la table : $ajoutTable <br/>";
+		echo "Nombre d'image non presente dans la table : $nbErreurImg <br/><br/>";
+		echo $text;
+
+    
 
 		include("script_menu.php");
 	?>
