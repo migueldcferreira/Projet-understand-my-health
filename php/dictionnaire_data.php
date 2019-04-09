4db24bae555f3cc09cf924ae103286f8836ce7f2
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-04-09 18:40:13 +0200

    Affiche [Image] s'il n'y a qu'une img pour une def

--- php/dictionnaire_data.php
+++ php/dictionnaire_data.php
@@ -20,6 +20,8 @@ $columns = array(
 // getting total number records without any search
 $sql = "SELECT MOT, DEFINITION";
 $sql.=" FROM TABLE_DEFINITION WHERE A_CONFIRMER=0 AND CLASSEMENT=1";
+$sql.=" UNION DISTINCT SELECT MOT, '[Image]' AS DEFINITION";
+$sql.=" FROM TABLE_LIEN_MOT_IMAGE WHERE AND CLASSEMENT=1";
 $query=  $db->prepare($sql) ;
 $query ->execute() or die("dictionnaire_data.php: get defs 1");
 $totalData = $query->rowCount();
@@ -28,6 +30,8 @@ $totalFiltered = $totalData;  // when there is no search parameter then total nu
 
 $sql = "SELECT MOT, DEFINITION";
 $sql.=" FROM TABLE_DEFINITION WHERE A_CONFIRMER=0 AND CLASSEMENT=1";
+$sql.=" UNION DISTINCT SELECT MOT, '[Image]' AS DEFINITION";
+$sql.=" FROM TABLE_LIEN_MOT_IMAGE WHERE AND CLASSEMENT=1";
 if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
 	$sql.=" AND MOT LIKE '%".$requestData['search']['value']."%' ";    
 	
