81007a74fbf1122aa5ed6a1893b48235131d9192
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-04-09 19:01:17 +0200

    correction recherche

--- php/dictionnaire_data.php
+++ php/dictionnaire_data.php
@@ -30,14 +30,15 @@ $totalFiltered = $totalData;  // when there is no search parameter then total nu
 
 $sql = "SELECT MOT, DEFINITION";
 $sql.=" FROM TABLE_DEFINITION WHERE A_CONFIRMER=0 AND CLASSEMENT=1";
+if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
+	$sql.=" AND MOT LIKE '%".$requestData['search']['value']."%' ";    
+}
 $sql.=" UNION DISTINCT SELECT MOT, '[Image]' AS DEFINITION";
 $sql.=" FROM TABLE_LIEN_MOT_IMAGE WHERE CLASSEMENT=1";
 if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
 	$sql.=" AND MOT LIKE '%".$requestData['search']['value']."%' ";    
-	
-
-	
 }
+
 $query= $db->prepare($sql) ;
 $query ->execute() or die("dictionnaire_data.php: get defs 2");
 $totalFiltered = $query->rowCount(); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
