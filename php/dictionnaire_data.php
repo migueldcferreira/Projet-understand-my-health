5c1a0a1b1a6a0bc83f2596c3de2faeb0690ef6d5
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-04-09 19:35:15 +0200

    def = [Image] que si ya une img et pas de def

--- php/dictionnaire_data.php
+++ php/dictionnaire_data.php
@@ -18,9 +18,9 @@ $columns = array(
 );
 
 // getting total number records without any search
-$sql = "SELECT MOT, DEFINITION";
+$sql = "SELECT MOT";
 $sql.=" FROM TABLE_DEFINITION WHERE A_CONFIRMER=0 AND CLASSEMENT=1";
-$sql.=" UNION DISTINCT SELECT MOT, '[Image]' AS DEFINITION";
+$sql.=" UNION DISTINCT SELECT MOT";
 $sql.=" FROM TABLE_LIEN_MOT_IMAGE WHERE CLASSEMENT=1";
 $query=  $db->prepare($sql) ;
 $query ->execute() or die("dictionnaire_data.php: get defs 1");
@@ -33,12 +33,13 @@ $sql.=" FROM TABLE_DEFINITION WHERE A_CONFIRMER=0 AND CLASSEMENT=1";
 if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
 	$sql.=" AND MOT LIKE '%".str_replace("'","''",$requestData['search']['value'])."%' ";    
 }
-$sql.=" UNION DISTINCT SELECT MOT, '[Image]' AS DEFINITION";
+$sql.=" UNION SELECT MOT, '[Image]' AS DEFINITION";
 $sql.=" FROM TABLE_LIEN_MOT_IMAGE WHERE CLASSEMENT=1";
 if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
 	$sql.=" AND MOT LIKE '%".str_replace("'","''",$requestData['search']['value'])."%' ";    
 }
-
+$sql.=" AND MOT NOT IN";
+$sql.=" SELECT DISTINCT MOT FROM TABLE_DEFINITION";
 $query= $db->prepare($sql) ;
 $query ->execute() or die("dictionnaire_data.php: get defs 2");
 $totalFiltered = $query->rowCount(); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
