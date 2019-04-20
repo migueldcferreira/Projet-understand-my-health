0aadcedb36b621cc5fe81ad10bcc55caab10197b
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-04-20 16:35:20 +0200

    Create cgu.php

---
+++ php/cgu.php
@@ -0,0 +1,27 @@
+<?php 
+      include('Bdd_interaction.php'); 
+<!DOCTYPE html>
+<html>
+<head>
+<?php include("head.php"); ?>
+<title>Modifier le mot de passe</title>
+     <meta charset="utf-8">
+     <link rel="stylesheet" type="text/css" href="..\css/form_inscr.css">
+     
+  
+</head>
+
+<body>
+  <?php include("menu.php"); ?>
+
+  <div class="header form_head bg-success">  
+    <h2>Termes et conditions d'utilisation</h2>
+  </div>
+
+
+
+
+
+  <?php include("script_menu.php"); ?>
+</body>
+</html>
