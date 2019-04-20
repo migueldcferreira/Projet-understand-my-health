f858da67d77e008d69d93dc16094e2db422c468c
TheoDelacoux <44262754+TheoDelacoux@users.noreply.github.com>
2019-04-20 16:50:00 +0200

    Update cgu.php

--- php/cgu.php
+++ php/cgu.php
@@ -7,7 +7,7 @@
 <?php include("head.php"); ?>
 <title>Modifier le mot de passe</title>
      <meta charset="utf-8">
-     <link rel="stylesheet" type="text/css" href="..\css/form_inscr.css">
+     <link rel="stylesheet" href="../css/dictionnaireIllustre.css" />
      
   
 </head>
@@ -16,7 +16,7 @@
   <?php include("menu.php"); ?>
 
   <div class="header form_head bg-success">  
-    <h2>Termes et conditions d'utilisation</h2>
+    <h1 class="title">Termes et conditions d'utilisation</h1>
   </div>
 
 
